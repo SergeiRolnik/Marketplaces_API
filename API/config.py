dfc7d3668f8d1f8dfda1b134d1409378a997ea14
Сергей Рольник <forgedwheels@gmail.com>
2022-07-20 16:32:46 +0300

    version 1.3 added

--- API/config.py
+++ API/config.py
@@ -23,7 +23,7 @@ host = 'rc1b-itt1uqz8cxhs0c3d.mdb.yandexcloud.net',
 port = '6432',
 dbname = 'market_db',
 user = 'srolnik',
-password = 'Qazwsx123Qaz',
+password = '',
 target_session_attrs = 'read-write',
 sslmode = 'verify-full'
 """
